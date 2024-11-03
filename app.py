e04ece72414bd480ff298c8eac8a1074ee0328d3
Paul Koester <paulhkoester@gmail.com>
2024-11-03 01:48:45 -0600

    add overview

--- app.py
+++ app.py
@@ -91,6 +91,83 @@ def get_row_read():
 
     return result
 
+@app.route("/overview-event-occs/", methods=["GET"])
+def overview_event_occs():
+    db_handler = db_factory(DB_FILE)
+    resolver = Resolver(db_handler)
+    summaries = resolver.overview_event_occs().to_dict(orient="records")
+    return render_template("overview_event_occs.html", summaries=summaries)
+
+
+# @app.route("/overview-performed-songs/", methods=["GET"])
+# def overview_performed_songs():
+#     db_handler = db_factory(DB_FILE)
+#     resolver = Resolver(db_handler)
+#     summaries = resolver.overview_performed_songs().to_dict(orient="records")
+#     return render_template("overview_performed_songs.html", summaries=summaries)
+
+
+# @app.route("/overview-songs/", methods=["GET"])
+# def overview_songs():
+#     db_handler = db_factory(DB_FILE)
+#     resolver = Resolver(db_handler)
+#     summaries = resolver.overview_songs().to_dict(orient="records")
+#     return render_template("overview_songs.html", summaries=summaries)
+
+
+# @app.route("/overview-people/", methods=["GET"])
+# def overview_people():
+#     db_handler = db_factory(DB_FILE)
+#     resolver = Resolver(db_handler)
+#     summaries = resolver.overview_people().to_dict(orient="records")
+#     return render_template("overview_people.html", summaries=summaries)
+
+
+# @app.route("/detail-event-occ/", methods=["GET", "POST"])
+# def detail_event_occ():
+#     db_handler = db_factory(DB_FILE)
+#     return render_template("detail_event_occ.html")
+
+
+# @app.route("/detail-performed-song/", methods=["GET", "POST"])
+# def detail_performed_song():
+#     db_handler = db_factory(DB_FILE)
+#     return render_template("detail_performed_song.html")
+
+
+# @app.route("/detail-song/", methods=["GET", "POST"])
+# def detail_performed_song():
+#     db_handler = db_factory(DB_FILE)
+#     return render_template("detail_song.html")
+
+
+@app.route("/detail-person/<string:person_id>")
+def detail_person(person_id):
+    db_handler = db_factory(DB_FILE)
+    resolver = Resolver(db_handler)
+    person = resolver._person_mapping().loc[person_id].to_dict()
+
+    contacts = []
+    facebook = person.pop("facebook", "")
+    if facebook != "":
+        contacts.append(["Facebook", facebook])
+    insta = person.pop("insta", "")
+    if insta != "":
+        contacts.append(["Instagram", insta])
+    youtube = person.pop("youtube", "")
+    if youtube != "":
+        contacts.append(["YouTube", youtube])
+    other_contact = person.pop("other_contact", "")
+    if other_contact != "":
+        contacts.append(["Other Contact", other_contact])
+    person["contacts"] = contacts
+
+    return render_template("detail_person.html", person=person)
+
+# @app.route("/detail-venue/", methods=["GET", "POST"])
+# def detail_venue():
+#     db_handler = db_factory(DB_FILE)
+#     return render_template("detail_venue.html")
 
 
 if __name__ == '__main__':
