08739634d367a9144bb2c249075e58fd9c60f9ab
Paul Koester <paulhkoester@gmail.com>
2024-11-05 21:14:56 -0600

    move to globals

--- jamdb/globals.py
+++ jamdb/globals.py
@@ -7,3 +7,17 @@ DB_FILE = DATA_DIR / "jamming.db"
 
 TEST_DATA_DIR = Path("data/testing")
 TEST_DB_FILE = TEST_DATA_DIR / "jamming.db"
+
+def _id_from_name(name):
+    return name.lower().strip().replace(" ", "_")
+
+def _format_id_as_str(x):
+    # Some ids are strs that look like nums, and pandas will cast them to float.
+    # This undoes that.
+    try:
+        x = float(x)
+        x = f"{x:.0f}"
+    except ValueError:
+        pass
+    return x
+    
