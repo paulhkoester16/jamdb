8e161ad8d1cf13b039fa7a13869f83c5a819c010
Paul Koester <paulhkoester@gmail.com>
2024-11-16 15:38:53 -0600

    update graphene resolvers

--- jamdb/graphene.py
+++ jamdb/graphene.py
@@ -4,9 +4,8 @@ from sqlalchemy.ext.automap import automap_base
 import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
-from .transformations import format_id_as_str
-
-
+from .globals import DB_FILE
+from .transformations import format_id_as_str, create_embed_link
 
 
 def _automap_sqlalchemy_models(sqlalchemy_engine):
@@ -62,11 +61,34 @@ def _create_qraphene_objects(model_classes):
             return cls
         return __inner_factory_function
 
-    
+
+    def _personinstruments_to_instruments(personinstruments):
+        insts = {
+            pers_inst.instrument.id: pers_inst.instrument
+            for pers_inst in personinstruments
+        }
+        insts = sorted(list(insts.values()), key=lambda inst: inst.id)
+        return insts
+
+
+    class PlayerGQL(graphene.ObjectType): 
+        person = graphene.Field(lambda: PersonGQL)
+        instruments = graphene.List(lambda: InstrumentGQL)
+        instrument_list = graphene.List(graphene.String)
+
+        def resolve_instrument_list(root, info):
+            return [inst.instrument for inst in root.instruments]
+
+
     @register_gql("chart")
     class ChartGQL(SQLAlchemyObjectType):
         class Meta:
             model = model_classes["Chart"]
+
+        embeddable_link = graphene.String()
+        
+        def resolve_embeddable_link(root, info):
+            return create_embed_link(root.source, root.link)
 
     
     @register_gql("composer") 
@@ -103,11 +125,39 @@ def _create_qraphene_objects(model_classes):
             model = model_classes["EventOcc"]
     
         song_performs = graphene.List(lambda: SongPerformGQL)
-    
+        venue = graphene.Field(lambda: VenueGQL)
+        players = graphene.List(lambda: PlayerGQL)
+        
         def resolve_song_performs(root, info):
             return root.songperform_collection
-    
 
+        def resolve_venue(root, info):
+            return root.eventgen.venue
+
+        def resolve_players(root, info):
+            # TODO:  refactor using `_personinstruments_to_instruments`
+            
+            players_dict = {}
+            for song in root.songperform_collection:        
+                for perf in song.songperformer_collection:
+                    pers_ins = perf.personinstrument
+                    person_id = pers_ins.person.id
+                    if person_id not in players_dict:
+                        players_dict[person_id] = {
+                            "person": pers_ins.person,
+                            "instruments": {}
+                        }
+                    inst = pers_ins.instrument
+                    players_dict[person_id]["instruments"][inst.id] = inst
+            for player in players_dict.values():
+                player["instruments"] = sorted(
+                    list(player["instruments"].values()),
+                    key=lambda inst: inst.id
+                )
+            players = [PlayerGQL(**player) for player in players_dict.values()]
+            return players
+
+    
     @register_gql("genre")
     class GenreGQL(SQLAlchemyObjectType):
         class Meta:
@@ -181,7 +231,11 @@ def _create_qraphene_objects(model_classes):
     class PerformanceVideoGQL(SQLAlchemyObjectType):
         class Meta:
             model = model_classes["PerformanceVideo"]
-    
+
+        embeddable_link = graphene.String()
+
+        def resolve_embeddable_link(root, info):
+            return create_embed_link(root.source, root.link)
 
     @register_gql("person")
     class PersonGQL(SQLAlchemyObjectType):
@@ -191,7 +245,21 @@ def _create_qraphene_objects(model_classes):
         contacts = graphene.List(lambda: ContactGQL)
         event_gens = graphene.List(lambda: EventGenGQL)
         person_instruments = graphene.List(lambda: PersonInstrumentGQL)
-    
+        instruments = graphene.List(lambda: InstrumentGQL)
+        instrument_list = graphene.List(graphene.String)
+        combined_name = graphene.String()
+
+        events_attended = graphene.List(lambda: EventOccGQL)        
+        song_performs = graphene.List(lambda: SongPerformGQL)
+        songs_performed_with = graphene.Field(
+            graphene.List(lambda: SongPerformGQL),
+            other_person_id=graphene.ID()
+        )
+        songs_performed_without = graphene.Field(
+            graphene.List(lambda: SongPerformGQL),
+            other_person_id=graphene.ID()
+        )
+
         def resolve_contacts(root, info):
             return root.contact_collection
     
@@ -201,6 +269,61 @@ def _create_qraphene_objects(model_classes):
         def resolve_person_instruments(root, info):
             return root.personinstrument_collection
 
+        def resolve_combined_name(root, info):
+            result = root.public_name
+            if root.full_name != root.public_name:
+                result += f" ({root.full_name})"
+            return result
+
+        def resolve_instruments(root, info):
+            insts = _personinstruments_to_instruments(root.personinstrument_collection)            
+            return insts
+
+        def resolve_instrument_list(root, info):
+            insts = _personinstruments_to_instruments(root.personinstrument_collection)
+            return [inst.instrument for inst in insts]
+
+        def resolve_song_performs(root, info):
+            perfs = {}
+            for persinst in root.personinstrument_collection:
+                for perf in persinst.songperformer_collection:
+                    songperf = perf.songperform
+                    perfs[songperf.id] = songperf
+            return sorted(list(perfs.values()), key=lambda x: (x.eventocc.date, x.song_id))
+
+        def resolve_events_attended(root, info):
+            events = {}
+            for persinst in root.personinstrument_collection:
+                for perf in persinst.songperformer_collection:
+                    event = perf.songperform.eventocc
+                    events[event.id] = event
+            return sorted(list(events.values()), key=lambda x: x.date)
+
+        def resolve_songs_performed_with(root, info, other_person_id):
+            # TODO - refactor commonalities out of `resolve_songs_performed_with`
+            #        and `resolve_songs_performed_without`
+            all_performed_songs = PersonGQL.resolve_song_performs(root, info)
+            with_other = []
+            for song in all_performed_songs:
+                song_players = [
+                    performer.personinstrument.person.id
+                    for performer in song.songperformer_collection
+                ]
+                if other_person_id in song_players:
+                    with_other.append(song)
+            return with_other
+
+        def resolve_songs_performed_without(root, info, other_person_id):
+            all_performed_songs = PersonGQL.resolve_song_performs(root, info)
+            without_other = []
+            for song in all_performed_songs:
+                song_players = [
+                    performer.personinstrument.person.id
+                    for performer in song.songperformer_collection
+                ]
+                if other_person_id not in song_players:
+                    without_other.append(song)
+            return without_other
 
     @register_gql("person_instrument")
     class PersonInstrumentGQL(SQLAlchemyObjectType):
@@ -208,15 +331,32 @@ def _create_qraphene_objects(model_classes):
             model = model_classes["PersonInstrument"]
     
         song_performers = graphene.List(lambda: SongPerformerGQL)
-    
+        song_performs = graphene.List(lambda: SongPerformGQL)
+        events_attended = graphene.List(lambda: EventOccGQL)
+        
         def resolve_song_performers(root, info):
             return root.songperformer_collection
-    
 
+        def resolve_song_performs(root, info):
+            return [x.songperform for x in root.songperformer_collection]
+
+        def resolve_events_attended(root, info):
+            events = {}
+            for perf in root.songperformer_collection:
+                event = perf.songperform.eventocc
+                events[event.id] = event
+            return sorted(list(events.values()), key=lambda x: x.date)
+
+    
     @register_gql("ref_ref")
     class RefRecGQL(SQLAlchemyObjectType):
         class Meta:
             model = model_classes["RefRec"]
+
+        embeddable_link = graphene.String()
+
+        def resolve_embeddable_link(root, info):
+            return create_embed_link(root.source, root.link)
     
 
     @register_gql("setlist")
@@ -267,7 +407,7 @@ def _create_qraphene_objects(model_classes):
     class SongLearnGQL(SQLAlchemyObjectType):
         class Meta:
             model = model_classes["SongLearn"]
-    
+
 
     @register_gql("song_perform")
     class SongPerformGQL(SQLAlchemyObjectType):
@@ -276,19 +416,46 @@ def _create_qraphene_objects(model_classes):
     
         performance_videos = graphene.List(lambda: PerformanceVideoGQL)
         song_performers = graphene.List(lambda: SongPerformerGQL)
-    
+        song_perform_name = graphene.String()
+        players = graphene.List(lambda: PlayerGQL)
+        
         def resolve_performance_videos(root, info):
             return root.performancevideo_collection
     
         def resolve_song_performers(root, info):
             return root.songperformer_collection
-    
+
+        def resolve_song_perform_name(root, info):
+            return f"{root.song.song} ({root.eventocc.name})"
+
+        def resolve_players(root, info):
+            # TODO:  refactor using `_personinstruments_to_instruments`            
+            players_dict = {}
+            for perf in root.songperformer_collection:
+                pers_ins = perf.personinstrument
+                person_id = pers_ins.person.id
+                if person_id not in players_dict:
+                    players_dict[person_id] = {
+                        "person": pers_ins.person,
+                        "instruments": {}
+                    }
+                inst = pers_ins.instrument
+                players_dict[person_id]["instruments"][inst.id] = inst
+            for player in players_dict.values():
+                player["instruments"] = sorted(
+                    list(player["instruments"].values()),
+                    key=lambda inst: inst.id
+                )
+            players = [PlayerGQL(**player) for player in players_dict.values()]
+            return players
+                
+
 
     @register_gql("song_performer")
     class SongPerformerGQL(SQLAlchemyObjectType):
         class Meta:
             model = model_classes["SongPerformer"]
-    
+
 
     @register_gql("subgenre")
     class SubgenreGQL(SQLAlchemyObjectType):
@@ -322,15 +489,12 @@ def _create_qraphene_objects(model_classes):
         hosted_event_series = graphene.List(lambda: EventGenGQL)
     
         def resolve_zip(root, info):
-            return _format_id_as_str(root.zip)
+            return format_id_as_str(root.zip)
         
         def resolve_address_string(root, info):
             return f"{root.address}, {root.city}, {root.state}, {VenueGQL.resolve_zip(root, info)}"
     
         def resolve_google_map_string(root, info):
-            # GrapheneObject interpret resolvers as static methods -- there is no `self` or `cls`
-            # which can easily access other methods in here, so need to access other 
-            # resolvers by explicitly naming cls
             address_string = VenueGQL.resolve_address_string(root, info)
             return f"https://www.google.com/maps/place/{address_string}".replace(" ", "+")
         
@@ -393,10 +557,10 @@ def _query_factory(gql_classes):
         instrument = graphene.Field(InstrumentGQL, id=graphene.ID())
     
         keys = graphene.List(KeyGQL)
-        key = graphene.Field(KeyGQL, key_id=graphene.ID())
+        key = graphene.Field(KeyGQL, id=graphene.ID())
     
         modes = graphene.List(ModeGQL)
-        mode = graphene.Field(ModeGQL, mode_id=graphene.ID())
+        mode = graphene.Field(ModeGQL, id=graphene.ID())
     
         performance_videos = graphene.List(PerformanceVideoGQL)
         performance_video = graphene.Field(PerformanceVideoGQL, id=graphene.ID())
@@ -423,7 +587,7 @@ def _query_factory(gql_classes):
         song_perform = graphene.Field(SongPerformGQL, id=graphene.ID())
     
         song_performers = graphene.List(SongPerformerGQL)
-        song_performer = graphene.Field(SongPerformerGQL, d=graphene.ID())
+        song_performer = graphene.Field(SongPerformerGQL, id=graphene.ID())
     
         subgenres = graphene.List(SubgenreGQL)
         subgenre = graphene.Field(SubgenreGQL, id=graphene.ID())
@@ -455,19 +619,19 @@ def _query_factory(gql_classes):
         def resolve_event_gens(root, info):
             return EventGenGQL.get_query(info).all()
     
-        def resolve_event_gen(root, info):
+        def resolve_event_gen(root, info, id):
             return EventGenGQL.get_node(info, id)
     
         def resolve_event_occs(root, info):
             return EventOccGQL.get_query(info).all()
     
-        def resolve_event_occ(root, info):
+        def resolve_event_occ(root, info, id):
             return EventOccGQL.get_node(info, id)
     
         def resolve_genres(root, info):
             return GenreGQL.get_query(info).all()
     
-        def resolve_genre(root, info):
+        def resolve_genre(root, info, id):
             return GenreGQL.get_node(info, id)
     
         def resolve_instruments(root, info):
@@ -479,7 +643,7 @@ def _query_factory(gql_classes):
         def resolve_keys(root, info):
             return KeyGQL.get_query(info).all()
     
-        def resolve_key(root, info):
+        def resolve_key(root, info, id):
             return KeyGQL.get_node(info, id)
     
         def resolve_modes(root, info):
@@ -491,7 +655,7 @@ def _query_factory(gql_classes):
         def resolve_performance_videos(root, info):
             return PerformanceVideoGQL.get_query(info).all()
     
-        def resolve_performance_video(root, info):
+        def resolve_performance_video(root, info, id):
             return PerformanceVideoGQL.get_node(info, id)
     
         def resolve_person_instruments(root, info):
@@ -533,7 +697,7 @@ def _query_factory(gql_classes):
         def resolve_song_performs(root, info):
             return SongPerformGQL.get_query(info).all()
     
-        def resolve_song_perform(root, info):
+        def resolve_song_perform(root, info, id):
             return SongPerformGQL.get_node(info, id)
     
         def resolve_song_performers(root, info):
@@ -578,9 +742,11 @@ class GrapheneSQLSession:
         self.schema = schema
 
     @classmethod
-    def from_sqlite_file(cls, sqlite_file):
-    
-        engine = sqlalchemy.create_engine(f'sqlite:///{sqlite_file}')
+    def from_sqlite_file(cls, sqlite_file=DB_FILE):
+        engine = sqlalchemy.create_engine(
+            f'sqlite:///{sqlite_file}',
+            connect_args={'check_same_thread': False}
+        )
         schema = get_graphene_schema(engine)
         
         Session = sessionmaker(bind=engine)
@@ -588,10 +754,8 @@ class GrapheneSQLSession:
 
         return cls(session=session, schema=schema)
 
-    def execute(self, query):
-        return self.schema.execute(query, context_value={'session': self.session})
-
-
+    def execute(self, query, variables=None):
+        return self.schema.execute(query, variables=variables, context_value={'session': self.session})
 
 
 
@@ -655,19 +819,19 @@ def _query_factory_broken(gql_classes):
         def resolve_event_gens(root, info):
             return EventGenGQL.get_query(info).all()
     
-        def resolve_event_gen(root, info):
+        def resolve_event_gen(root, info, id):
             return EventGenGQL.get_node(info, id)
     
         def resolve_event_occs(root, info):
             return EventOccGQL.get_query(info).all()
     
-        def resolve_event_occ(root, info):
+        def resolve_event_occ(root, info, id):
             return EventOccGQL.get_node(info, id)
     
         def resolve_genres(root, info):
             return GenreGQL.get_query(info).all()
     
-        def resolve_genre(root, info):
+        def resolve_genre(root, info, id):
             return GenreGQL.get_node(info, id)
     
         def resolve_instruments(root, info):
@@ -679,7 +843,7 @@ def _query_factory_broken(gql_classes):
         def resolve_keys(root, info):
             return KeyGQL.get_query(info).all()
     
-        def resolve_key(root, info):
+        def resolve_key(root, info, id):
             return KeyGQL.get_node(info, id)
     
         def resolve_modes(root, info):
@@ -691,7 +855,7 @@ def _query_factory_broken(gql_classes):
         def resolve_performance_videos(root, info):
             return PerformanceVideoGQL.get_query(info).all()
     
-        def resolve_performance_video(root, info):
+        def resolve_performance_video(root, info, id):
             return PerformanceVideoGQL.get_node(info, id)
     
         def resolve_person_instruments(root, info):
@@ -733,7 +897,7 @@ def _query_factory_broken(gql_classes):
         def resolve_song_performs(root, info):
             return SongPerformGQL.get_query(info).all()
     
-        def resolve_song_perform(root, info):
+        def resolve_song_perform(root, info, id):
             return SongPerformGQL.get_node(info, id)
     
         def resolve_song_performers(root, info):
